6b1c25a5f3805ae6584a29cf4a06d49e0838fae2
Zakinonstop <zakinonstop1@gmail.com>
2022-11-30 23:32:48 +0700

    first commit

---
+++ application/controllers/Baru.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Baru extends CI_Controller {
+
+	public function index()
+	{
+        $data = [
+            'getApi'=> json_decode($this->url_get_contents()),
+        ];
+        $this->load->view('baru', $data);
+	}
+
+    function url_get_contents()
+    {
+        if (!function_exists('curl_init')) {
+            die('CURL is not installed!');
+        }
+
+        $ch = curl_init();
+        $post = ['id' => 'BENGKULU'];
+        $username = 'outdoor';
+        $password = 'qmzDaFwbqRtXx39q';
+        curl_setopt($ch, CURLOPT_URL, 'https://ispu.menlhk.go.id/api/public/outdoor');
+        curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);
+        curl_setopt_array($ch, array(CURLOPT_RETURNTRANSFER => TRUE));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return $output;
+    }
+}
---
+++ application/views/baru.php
@@ -0,0 +1,182 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Kementrian Lingkungan Hidup</title>
+    <link rel="icon" href="<?= base_url('assets/img/logo.png')?>" type="image/icon type">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= base_url('assets/css/style.css')?>">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+  </head>
+
+  <body>
+    <section id="main" style="display: block;">
+        <div class="container-fluid layout-baru" style="background-color: #242948; height: 240cm;">
+         <div class="d-flex justify-content-between navbar-1-baru">
+            <div class="d-flex justify-content-center">
+                <div class="">
+                    <img src="<?= base_url('assets/img/logo.png')?>" alt="" width="500" height="500">
+                </div>
+                <div class="klu-kiri">
+                    <span class="text-center text-white">KEMENTERIAN <br>LINGKUNGAN HIDUP <br>DAN KEHUTANAN</span>
+                </div>
+            </div>
+            <div class="d-flex justify-content-center">
+                <div class="">
+                    <img src="<?= base_url('assets/img/logo-bengkulu.png')?>" alt="" width="500" height="500">
+                </div>
+                <div class="klu-kanan">
+                    <span class="text-center text-white">PEMERINTAH <br>KOTA BENGKULU</span>
+                </div>
+            </div>
+         </div>
+
+         <div class="d-flex justify-content-center navbar-2-baru">
+            <div class="d-flex justify-content-center">
+                <div class="kualitas-text text-center">
+                    <span class="text-white">KUALITAS UDARA <br>KOTA BENGKULU</span>
+                </div>
+            </div>    
+         </div>
+
+         <div class="d-flex justify-content-center navbar-3-baru">
+            <div class="d-flex justify-content-center">
+                <div class="jam-text text-center">
+                    <span class="text-white jam-text-value"><?= substr($getApi->data->waktu,0, -10)?><br><?= substr($getApi->data->waktu, -5)?> WIB</span>
+                </div>
+            </div>    
+         </div>
+
+         <div class="d-flex justify-content-center navbar-4-baru">
+            <div class="d-flex justify-content-center">
+                <div class="number-text text-center" style="background-color: green;">
+                    <h1 class="number-text-number text-center text-white" style="font-size: 40vw;"><?= $getApi->data->o3?></h1>
+                </div>
+            </div>    
+         </div>
+
+         <div class="d-flex justify-content-center navbar-5-baru">
+            <div class="d-flex justify-content-center">
+                <div class="category-text category-nya text-center">
+                    <input hidden class="category " type="text" value="<?= $getApi->data->c_o3?>">
+                    <span class="text-white baik-text"></span>
+                </div>
+            </div>    
+         </div>
+
+         <div class="row navbar-6-baru">
+            <div class="col-kiri col-6" style="padding-right: 10cm;">
+            <div class="tes">
+                <div class="param-text">
+                    <span class="text-left text-white" style="font-size: 3.5vw; font-weight: 600;">Parameter Kritis</span>
+                </div>
+                <div class="d-flex justify-content-end value-text">
+                    <span class="" style="font-size: 8vw; font-weight: 600; color: yellow; margin-top: -1cm" align="right">O<sub>3</sub></span>
+                </div>
+            </div>
+            </div>
+            <div class="col-kanan col-6" style="padding-left: 10cm;">
+                <div class="tes2">
+                    <div class="param-text">
+                        <span class="text-left text-white" style="font-size: 3.5vw; font-weight: 600;">Nilai Konsentrasi</span>
+                    </div>
+                    <div class="d-flex justify-content-end value-text" style="margin-top: -3cm;">
+                        <span class="konsentrasi-text" style="font-size: 8vw; font-weight: 600; color: yellow;" align="right"><?= $getApi->data->o3; ?></span>
+                    </div>
+                    <div class="d-flex justify-content-end value-text" style="margin-top: -5cm;">
+                        <span class="text-white" style="font-size: 3vw;" align="right">ug/m<sup>3</sup></span>
+                    </div>
+                </div>
+            </div>
+         </div>
+        </div>
+    </section>
+
+    <section id="errorya" style="display: none;">
+        <div class="errornya">
+            <span style="font-size: 20vw;">Tidak ada koneksi Internet !</span>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script>
+        $('document').ready(function () {
+            // cek while no internet, show text no connection 
+            noInternet();
+            function noInternet() {
+                var error_text = $('.number-text-number h4').html();
+                if (error_text == 'A PHP Error was encountered') {
+                    $('.jam-text-value').html('---');
+                    $('.number-text-number').html('');
+                    $('.baik-text').html('---');
+                    $('.konsentrasi-text').html('---');
+                    $('.category-nya').html('<span class="text-white text-center baik-text">Tidak Ada Koneksi Internet !</span>');
+                }else {
+                    $('#main').css('display', 'block');
+                    $('#errorya').css('display', 'none');
+                }
+            }
+
+            // setInterval(ajaxCall, 300000); //300000 MS == 5 minutes
+            // setInterval(ajaxCall, 120000); //120000 MS == 2 minutes
+            
+            // set interval, reload page every 3 minutes 
+            setInterval(ajaxCall, 180000); //180000 MS == 3 minutes
+
+            // change color while category is different, follow the api 
+            changeColor();
+            function ajaxCall() {
+                window.location.reload()
+            }
+
+            function changeColor() {
+                var category = $('.category').val();
+                if (category == 1) {
+                    $('.number-text').css('background-color', '');
+                    $('.number-text').css('background-color', 'green');
+                    $('.baik-text').html('Baik');
+                    $('.number-text-number').css('font-size', '');
+                    $('.number-text-number').css('font-size', '40vw');
+                    $('.number-text-number').css('margin-top', '');
+                }else if (category == 2) {
+                    $('.number-text').css('background-color', '');
+                    $('.number-text').css('background-color', 'blue');
+                    $('.baik-text').html('Sedang');
+                    $('.number-text-number').css('font-size', '');
+                    $('.number-text-number').css('font-size', '40vw');
+                    $('.number-text-number').css('margin-top', '');
+                } else if (category == 3) {
+                    $('.number-text').css('background-color', '');
+                    $('.number-text').css('background-color', 'yellow');
+                    $('.number-text-number').css('font-size', '');
+                    $('.number-text-number').css('font-size', '30vw');
+                    $('.number-text-number').css('margin-top', '1cm');
+                    $('.baik-text').html('Tidak Sehat');
+                }else if (category == 4) {
+                    $('.number-text').css('background-color', '');
+                    $('.number-text').css('background-color', 'red');
+                    $('.baik-text').html('Sangat Tidak Sehat');
+                    $('.number-text-number').css('font-size', '');
+                    $('.number-text-number').css('font-size', '30vw');
+                    $('.number-text-number').css('margin-top', '1cm');
+                }else if (category == 5) {
+                    $('.number-text').css('background-color', '');
+                    $('.number-text').css('background-color', 'black');
+                    $('.baik-text').html('Berbahaya');
+                    $('.number-text-number').css('font-size', '');
+                    $('.number-text-number').css('font-size', '30vw');
+                    $('.number-text-number').css('margin-top', '1cm');
+                }
+            } 
+        })
+    </script>
+  </body>
+</html>
